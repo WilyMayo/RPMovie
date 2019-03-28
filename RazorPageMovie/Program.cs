b34eaeb6a5bc24563063bc23f6fd7ec9d165997e
Wily Mayo <wilymayo@hotmail.com>
2019-03-28 03:02:04 -0400

    RazorPage Movie made.

--- RazorPageMovie/Program.cs
+++ RazorPageMovie/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using RazorPageMovie.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace RazorPageMovie
 {
@@ -14,7 +17,28 @@ namespace RazorPageMovie
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.
+                        GetRequiredService<RazorPageMovieContext>();
+                    context.Database.Migrate();
+                    Seeddata.Initialize(services);
+
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred sedding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
